ba29e9560e7a95412bb0452f8025dfea5683bf22
rotten <1132264073@qq.com>
2022-10-12 13:22:29 +0800

    登录界面

--- florist/florist_front/src/views/login/login.vue
+++ florist/florist_front/src/views/login/login.vue
@@ -1,10 +1,58 @@
 <template>
-  <div class="login">i'm login</div>
+  <div class="login">
+    <div class="longpic">
+      <div class="loginform">
+        <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item label="username:">
+            <el-input
+              v-model="form.username"
+              placeholder="Please enter user name"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="password:">
+            <el-input
+              v-model="form.userpwd"
+              placeholder="Please input password"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">login</el-button>
+            <a href="">Registered account</a>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        userpwd: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit!");
+    },
+  },
+};
 </script>
 
 <style>
+.longpic {
+  background-image: url("flowerpic.jpeg");
+}
+.loginform {
+  position: relative;
+  top: 50px;
+  margin-left: 70%;
+  width: 20%;
+  height: 300px;
+  margin-top: 100px;
+}
 </style>
