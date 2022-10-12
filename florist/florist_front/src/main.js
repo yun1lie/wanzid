6a64118cc14f25335d780367c36cca5dc688401a
rotten <1132264073@qq.com>
2022-10-12 11:25:25 +0800

    elementui

--- florist/florist_front/src/main.js
+++ florist/florist_front/src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+// 引入elementui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
