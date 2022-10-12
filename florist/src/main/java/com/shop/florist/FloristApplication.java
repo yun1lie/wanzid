461503021297be131e01aa9fba896daf5f51eb45
rotten <1132264073@qq.com>
2022-10-12 11:05:02 +0800

    springboot初始化

---
+++ florist/src/main/java/com/shop/florist/FloristApplication.java
@@ -0,0 +1,13 @@
+package com.shop.florist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloristApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloristApplication.class, args);
+    }
+
+}
