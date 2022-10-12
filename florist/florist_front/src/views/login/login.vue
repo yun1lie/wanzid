272367a17cb432750bede49a6d5b47a09d72cf33
rotten <1132264073@qq.com>
2022-10-12 14:08:02 +0800

    111

--- florist/florist_front/src/views/login/login.vue
+++ florist/florist_front/src/views/login/login.vue
@@ -2,21 +2,23 @@
   <div class="login">
     <div class="longpic">
       <div class="loginform">
-        <el-form ref="form" :model="form" label-width="80px">
-          <el-form-item label="username:">
+        <el-form ref="form" :rules="rules" :model="form" label-width="80px">
+          <el-form-item label="username:" prop="username">
             <el-input
               v-model="form.username"
               placeholder="Please enter user name"
             ></el-input>
           </el-form-item>
-          <el-form-item label="password:">
+          <el-form-item label="password:" prop="userpwd">
             <el-input
               v-model="form.userpwd"
               placeholder="Please input password"
             ></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="onSubmit">login</el-button>
+            <el-button type="primary" @click="onSubmit('form')"
+              >login</el-button
+            >
             <a href="">Registered account</a>
           </el-form-item>
         </el-form>
@@ -33,11 +35,31 @@ export default {
         username: "",
         userpwd: "",
       },
+      rules: {
+        username: [
+          { required: true, message: "enter one user name", trigger: "blur" },
+        ],
+        userpwd: [
+          {
+            required: true,
+            message: "enter one user password",
+            trigger: "blur",
+          },
+        ],
+      },
     };
   },
+
   methods: {
-    onSubmit() {
-      console.log("submit!");
+    onSubmit(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     },
   },
 };
@@ -46,13 +68,16 @@ export default {
 <style>
 .longpic {
   background-image: url("flowerpic.jpeg");
+  height: 300px;
 }
 .loginform {
+  padding: 20px;
+  background-color: aliceblue;
   position: relative;
   top: 50px;
   margin-left: 70%;
   width: 20%;
-  height: 300px;
+  height: 200px;
   margin-top: 100px;
 }
 </style>
