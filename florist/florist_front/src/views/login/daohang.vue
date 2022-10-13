f11451a98930b6cda1eae9c8d875ae10970fd61d
Agoni丶彼岸花 <910032214@qq.com>
2022-10-13 15:31:23 +0800

    改

--- florist/florist_front/src/views/login/daohang.vue
+++ florist/florist_front/src/views/login/daohang.vue
@@ -9,40 +9,19 @@
       <el-menu-item index="1" style="margin-left: 5%">
         <router-link to="/">首页</router-link>
       </el-menu-item>
-      <el-menu-item style="margin-left: 55%" index="2"
-        ><a href="https://www.ele.me" target="_blank">订单查询</a></el-menu-item
-      >
-      <el-submenu index="3">
-        <template slot="title">客户服务</template>
-        <el-menu-item index="3-1"
-          ><a class="a1" href="https://www.ele.me" target="_blank"
-            >在线支付</a
-          ></el-menu-item
-        >
-        <el-menu-item index="3-2"
-          ><a class="a1" href="https://www.ele.me" target="_blank"
-            >售后服务</a
-          ></el-menu-item
-        >
-        <el-menu-item index="3-3"
-          ><a class="a1" href="https://www.ele.me" target="_blank"
-            >留言反馈</a
-          ></el-menu-item
-        >
-      </el-submenu>
-      <el-menu-item index="4">
+      <el-menu-item index="2" style="margin-left: 70%">
         <router-link to="/ShoppingCart">Shopping Cart</router-link>
 
         <!-- <a href="https://www.ele.me" target="_blank"></a> -->
       </el-menu-item>
-      <el-menu-item v-if="userInfo == ''" index="5"
+      <el-menu-item v-if="userInfo == ''" index="3"
         ><router-link to="login">您好，请先登录</router-link></el-menu-item
       >
-      <el-menu-item v-if="userInfo != ''" index="5"
+      <el-menu-item v-if="userInfo != ''" index="3"
         ><router-link to="#">{{ userInfo.username }}</router-link></el-menu-item
       >
 
-      <el-menu-item index="6" v-if="userInfo == ''" style="margin-left: -10px"
+      <el-menu-item index="4" v-if="userInfo == ''" style="margin-left: -10px"
         ><router-link to="register">注册</router-link></el-menu-item
       >
     </el-menu>
