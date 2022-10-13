3a6b2cdc7a8bb37338ee363004fca1cce4068577
Agoni丶彼岸花 <910032214@qq.com>
2022-10-13 13:43:45 +0800

    圖片

--- florist.sql
+++ florist.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : my
+ Source Server         : 11
  Source Server Type    : MySQL
- Source Server Version : 80026
+ Source Server Version : 80031
  Source Host           : localhost:3306
  Source Schema         : florist
 
  Target Server Type    : MySQL
- Target Server Version : 80026
+ Target Server Version : 80031
  File Encoding         : 65001
 
- Date: 13/10/2022 13:09:07
+ Date: 13/10/2022 13:43:16
 */
 
 SET NAMES utf8mb4;
@@ -22,13 +22,25 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `flower`;
 CREATE TABLE `flower`  (
-  `fname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `fname` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
   `id` int(0) NOT NULL AUTO_INCREMENT,
-  `fdescribe` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `fdescribe` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
   `price` decimal(10, 2) NULL DEFAULT NULL,
-  `picUrl` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `picUrl` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of flower
+-- ----------------------------
+INSERT INTO `flower` VALUES (' Corolla Rose', 1, ' Allow you three lifetimes to accompany the twilight of the dynasty', 298.00, '1.jpg');
+INSERT INTO `flower` VALUES ('Pink carnation', 2, 'Gentle people are treasures on earth', 239.00, '2.jpg');
+INSERT INTO `flower` VALUES ('Rose tulips', 3, 'Eternal love. The love of a young girl is sincere and beautiful', 521.00, '3.jpg');
+INSERT INTO `flower` VALUES ('Sunflower', 4, ' Pick a ray of sunshine, make a bookmark, and warm every day.', 198.00, '4.jpg');
+INSERT INTO `flower` VALUES ('Lily', 5, 'I like you!Bless you ,Wish you the most beautiful place', 198.00, '5.jpg');
+INSERT INTO `flower` VALUES ('Broken Ice Blue Rose\n\n', 6, 'The hope for you is the stars and the sea', 469.00, '6.jpg');
+INSERT INTO `flower` VALUES ('Diana Pink Rose', 7, 'The sun caresses your face, never forgetting the beautiful picture; The hug you gave me accelerated my heartbeat. Happiness is so beautiful!', 599.00, '7.jpg');
+INSERT INTO `flower` VALUES ('Aisha Rose', 8, 'I want to spoil you as a princess and give you all the tenderness', 636.00, '8.jpg');
 
 -- ----------------------------
 -- Table structure for shoppingcart
@@ -38,7 +50,7 @@ CREATE TABLE `shoppingcart`  (
   `userId` int(0) NOT NULL,
   `flowerId` int(0) NOT NULL,
   PRIMARY KEY (`userId`, `flowerId`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for users
@@ -46,10 +58,10 @@ CREATE TABLE `shoppingcart`  (
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`  (
   `id` int(0) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of users
